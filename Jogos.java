b1d1508c0216ad3f9a47174ce88cb5027c9af5e1
Luiz Guilherme <101290653+LuizGuilhermecb99@users.noreply.github.com>
2022-10-03 12:28:29 -0300

    Create Jogos.java

---
+++ Jogos.java
@@ -0,0 +1,109 @@
+package Prateleiradejogos; 
+
+public class Jogos { 
+
+     
+
+   //Atributos 
+
+private String NameGame ; 
+
+private int ReleaseYear ; 
+
+private int HoursPlayed ; 
+
+  
+
+    //Métodos 
+
+    public String getNameGame() { 
+
+        return NameGame; 
+
+    } 
+
+  
+
+    public void setNameGame(String NameGame) { 
+
+        this.NameGame = NameGame; 
+
+    } 
+
+  
+
+    public int getReleaseYear() { 
+
+        return ReleaseYear; 
+
+    } 
+
+  
+
+    public void setReleaseYear(int ReleaseYear) { 
+
+        this.ReleaseYear = ReleaseYear; 
+
+    } 
+
+  
+
+    public int getHoursPlayed() { 
+
+        return HoursPlayed; 
+
+    } 
+
+  
+
+    public void setHoursPlayed(int HoursPlayed) { 
+
+        this.HoursPlayed = HoursPlayed; 
+
+    } 
+
+     
+
+    //Construtor 
+
+    public Jogos() { 
+
+         
+
+        this.HoursPlayed = HoursPlayed; 
+
+        this.NameGame = NameGame; 
+
+        this.ReleaseYear = ReleaseYear; 
+
+    } 
+
+  
+
+    
+
+  
+
+  
+
+     
+
+    
+
+     
+
+     
+
+     
+
+     
+
+     
+
+} 
+
+  
+
+  
+
+ 
